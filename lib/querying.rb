dc5b631c54fd845c2109fadc85ba49bf76559b15
Tomas Perez <tomasitopg6@gmail.com>
2020-07-31 16:15:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,7 @@ def select_series_title_with_most_human_characters
      INNER JOIN characters
        ON series.id = characters.series_id
      WHERE characters.species = 'human'
-     ORDER BY num_species DESC
+     ORDER BY species DESC
      LIMIT 1);"
 end
 
